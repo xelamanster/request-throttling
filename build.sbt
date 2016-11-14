9d6c78a7de422c11f1c64c321a5bfe1d17406800
xelamanster <xelamanster@gmail.com>
2016-11-14 02:48:31 +0200

    Started implementation of the tests.

--- build.sbt
+++ build.sbt
@@ -7,5 +7,5 @@ scalaVersion := "2.11.8"
 libraryDependencies ++= Seq(
   "org.scalactic" %% "scalactic" % "3.0.0",
   "org.scalatest" %% "scalatest" % "3.0.0" % "test",
-  "org.scalamock" %% "scalamock-scalatest-support" % "3.2.2" % "test"
+  "org.scalamock" %% "scalamock-scalatest-support" % "3.3.0" % "test"
 )
--- src/test/scala/TestSpec.scala
+++ src/test/scala/TestSpec.scala
@@ -1,6 +1,40 @@
+import org.scalamock.scalatest.MockFactory
 import org.scalatest.{FlatSpec, Matchers}
+import throttle.{Sla, SlaService, ThrottleServiceImpl, TimeProvider}
 
-class TestSpec extends FlatSpec with Matchers {
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
+class MyTestSpec extends FlatSpec with Matchers with MockFactory {
+  val myTimeProvider = new TimeProvider {
+    override def millis: Long = 0
+  }
 
+  "ThrottleService" should "call SlaService once for each user" in {
+    val user1 = "user1"
+    val user2 = "user2"
+
+    val slaStub = createSlaStub(1, user1, user2)
+    val throttle = new ThrottleServiceImpl(1, slaStub)
+
+    throttle.isRequestAllowed(Option(user1))
+    throttle.isRequestAllowed(Option(user2))
+    throttle.isRequestAllowed(Option(user1))
+    throttle.isRequestAllowed(Option(user2))
+
+    (slaStub.getSlaByToken _).verify(user1).once()
+    (slaStub.getSlaByToken _).verify(user2).once()
+  }
+
+  def createSlaStub(rps: Int, users: String*): SlaService = {
+    val serviceMock = stub[SlaService]
+    for(user <- users)
+      (serviceMock.getSlaByToken _)
+        .when(user)
+        .returning(
+          Future(Sla(user, rps))
+        )
+
+    serviceMock
+  }
 }
